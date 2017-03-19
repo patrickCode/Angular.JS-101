452b050f9f5f35f55b6658f9f433f35217e13edf
Raumali <raumali.nitd@gmail.com>
2017-03-20 00:20:06 +0530

    promises first commit

--- src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
+++ src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
@@ -1,61 +1,34 @@
 ﻿var app = angular.module('quest', []);
-//service
-//app.service('questionData', function () {
-//    this.getPublicQuestions = function () {
-//        return [
-//        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
-//        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
-//        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
-//        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
-//        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
-//        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() }];
-//    };
 
-//})
-
-//factory
-app.factory('questionData', function () {
+var questionData = function ($q, $http) {
     getPublicQuestions = function () {
-        return [
-        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
-        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
-        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
-        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
-        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
-        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() }];
-    };
-    return { getPublicQuestions: getPublicQuestions };
-})
+        var deferred = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'http://questhunt.azurewebsites.net/api/questions',
+        }).then(function (response) {
+            deferred.resolve(response.data);
+        }, function (error) {
+            deferred.reject(error)
+        });
 
-//app.config(function (questionDataProvider) {
-//    questionDataProvider.isDebugModeEnabled(true);
-//})
-//app.provider('questionData', function () {
-//    var isDebugMode = true;
-//    getPublicQuestions = function () {
-//        if (isDebugMode) { alert("Fetching From Provider"); }
-//        return [{
-//            question: "Name a few built-in providers in Angular.JS",
-//            questiontype: "text",
-//            answer: "$http",
-//            answertype: "subjective", createdOn: Date.now()
-//        }]
-//    }
-//    return {
-//        "isDebugModeEnabled": function (v) { isDebugMode = v; },
-//        "$get": function () {
-//            return {
-//                getPublicQuestions: getPublicQuestions
-//            }
-//        }
-//    }
-//})
-app.controller('questionlistctrl', function ($scope, questionData) {
+        return deferred.promise;
+    }
+    return { getPublicQuestions: getPublicQuestions }
+}
+app.factory('questiondata', questionData);
+app.controller('questionlistctrl', function ($scope, questiondata) {
     $scope.paneltitle = "My Questions";
-    $scope.questions = questionData.getPublicQuestions();
+    $scope.questions = [];
     $scope.questionheader = "Question";
     $scope.questiontypeheader = "Question Type";
     $scope.answertypeheader = "Answer Type";
     $scope.answerheader = "Answer";
     $scope.addeddateheader = "Added On";
+
+    questiondata.getPublicQuestions().then(function (response) {
+        $scope.totalCount = response.length;
+        $scope.questions = response;
+    });
+
 });

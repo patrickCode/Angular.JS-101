f8e15fa9f81357514725f70a95fb0373d9c0e7a6
Raumali <raumali.nitd@gmail.com>
2017-03-19 23:16:02 +0530

    controller-complete

--- src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
+++ src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
@@ -1,9 +1,25 @@
-﻿var app = angular.module('quest', []);
+﻿
+var app = angular.module('quest', []);
+
+
 app.controller('questionlistctrl', function ($scope) {
     $scope.paneltitle = "My Questions";
+    $scope.questions = [
+        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective" ,createdOn:Date.now() },
+        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
+        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
+        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() } ];
     $scope.questionheader = "Question";
     $scope.questiontypeheader = "Question Type";
     $scope.answertypeheader = "Answer Type";
     $scope.answerheader = "Answer";
     $scope.addeddateheader = "Added On";
+    
+
+
 });
+
+
+

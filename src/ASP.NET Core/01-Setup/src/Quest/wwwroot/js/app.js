81dc96181768b457f73d7ac105e859b7d9e9255c
Raumali <raumali.nitd@gmail.com>
2017-03-19 14:15:46 +0530

    controller-commit-2

--- src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
+++ src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
@@ -1,4 +1,9 @@
 ﻿var app = angular.module('quest', []);
 app.controller('questionlistctrl', function ($scope) {
-    $scope.paneltitle = "My Questions"
+    $scope.paneltitle = "My Questions";
+    $scope.questionheader = "Question";
+    $scope.questiontypeheader = "Question Type";
+    $scope.answertypeheader = "Answer Type";
+    $scope.answerheader = "Answer";
+    $scope.addeddateheader = "Added On";
 });

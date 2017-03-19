fc5d712c39aae52e604f778539621289ae2b0e0d
Raumali <raumali.nitd@gmail.com>
2017-03-20 00:06:02 +0530

    services-initial commit

--- src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
+++ src/ASP.NET Core/01-Setup/src/Quest/wwwroot/js/app.js
@@ -1,6 +1,58 @@
 ﻿var app = angular.module('quest', []);
-app.controller('questionlistctrl', function ($scope) {
+//service
+//app.service('questionData', function () {
+//    this.getPublicQuestions = function () {
+//        return [
+//        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+//        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
+//        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+//        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
+//        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+//        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() }];
+//    };
+
+//})
+
+//factory
+app.factory('questionData', function () {
+    getPublicQuestions = function () {
+        return [
+        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
+        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() },
+        { question: "Name a few built-in providers in Angular.JS", questiontype: "text", answer: "$http", answertype: "subjective", createdOn: Date.now() },
+        { question: "which of the following is not a built-in angular directive", questiontype: "text", answer: "ng-for", answertype: "Multi choice", createdOn: Date.now() }];
+    };
+    return { getPublicQuestions: getPublicQuestions };
+})
+
+//app.config(function (questionDataProvider) {
+//    questionDataProvider.isDebugModeEnabled(true);
+//})
+//app.provider('questionData', function () {
+//    var isDebugMode = true;
+//    getPublicQuestions = function () {
+//        if (isDebugMode) { alert("Fetching From Provider"); }
+//        return [{
+//            question: "Name a few built-in providers in Angular.JS",
+//            questiontype: "text",
+//            answer: "$http",
+//            answertype: "subjective", createdOn: Date.now()
+//        }]
+//    }
+//    return {
+//        "isDebugModeEnabled": function (v) { isDebugMode = v; },
+//        "$get": function () {
+//            return {
+//                getPublicQuestions: getPublicQuestions
+//            }
+//        }
+//    }
+//})
+app.controller('questionlistctrl', function ($scope, questionData) {
     $scope.paneltitle = "My Questions";
+    $scope.questions = questionData.getPublicQuestions();
     $scope.questionheader = "Question";
     $scope.questiontypeheader = "Question Type";
     $scope.answertypeheader = "Answer Type";

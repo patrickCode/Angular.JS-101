f5a40b0f3ed35d51d6dcf44784a9edd648c6b26f
Pratik Bhattacharya <pratikb@microsoft.com>
2017-03-20 11:25:40 +0530

    Dummy

---
+++ src/ASP.NET/WebApplication1/src/WebApplication1/wwwroot/js/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("app", []);
+
+app.controller("appCtrl", function ($scope) {
+    $scope.pageTitle = "My Angular Application1";
+});
+
+app.controller("appCtrl2", function ($scope) {
+    $scope.pageTitle = "My Angular Application2";
+})
